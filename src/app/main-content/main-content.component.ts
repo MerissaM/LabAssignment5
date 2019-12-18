19e9e71dc760ec35a450cb6afa1abe25a7d06d87
MerissaM <merissamani@mail.fresnostate.edu>
2019-12-18 14:19:44 -0800

    Lab Assignment 5 Completed

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+interface IStudent {
+  id: number;
+  firstName: string;
+  lastName: string;
+  course: string;
+  editMode: boolean;
+}
+
+
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html',
+  styleUrls: ['./main-content.component.css']
+})
+export class MainContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+  firstName = 'Merissa';
+  lastName = 'Muzafar';
+  greeting = 'r1c2';
+
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  showGreeting() {
+
+    this.greeting = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+  }
+  resetGreeting() {
+
+    this.greeting = 'r1c2';
+  }
+
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
+}
